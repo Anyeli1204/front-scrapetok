295ba3346f188bf467df4264c5110caf10c64cdd
Luis Cahuana <luis.cahuana@utec.edu.pe>
2025-05-28 19:34:38 -0500

    feat(domain): agregar entidades base del dominio (#33)

---
+++ src/main/java/org/example/scrapetok/domain/UserApifyCallHistorial.java
@@ -0,0 +1,31 @@
+package org.example.scrapetok.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@EqualsAndHashCode(exclude = "user")
+
+@Data
+public class UserApifyCallHistorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private GeneralAccount user;
+
+    // Pensamos expandir en un futuro el historial ...
+
+    @OneToMany(mappedBy = "historial", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserApifyFilters> filtros= new ArrayList<>();
+
+}
---
+++ src/main/java/org/example/scrapetok/domain/UserApifyFilters.java
@@ -0,0 +1,37 @@
+package org.example.scrapetok.domain;
+
+import com.example.scrapetok.domain.enums.ApifyRunStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class UserApifyFilters {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "historial_id", nullable = false)
+    private UserApifyCallHistorial historial;
+
+    // Cantidad de post por profile, hashtag o keyword -> Default: 1
+    private Integer nlastPostByHashtags;
+
+    @Column(columnDefinition = "TEXT")
+    private String hashtags;
+
+    @Column(columnDefinition = "TEXT")
+    private String keyWords;
+
+    @Column(columnDefinition = "TEXT")
+    private String tiktokAccount;
+    private String dateFrom;
+    private String dateTo;
+
+
+    @Enumerated(EnumType.STRING)
+    private ApifyRunStatus apifyRunStatus = ApifyRunStatus.FAILED;
+    private Integer executionTime;
+}
