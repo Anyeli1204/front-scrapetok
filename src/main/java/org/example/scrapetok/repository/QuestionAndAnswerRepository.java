a5cff481b27654435040491d3e05e56bb28b72ef
josue <josue.carrillo@utec.edu.pe>
2025-05-28 20:56:55 -0500

    feat: añadir enumeraciones base en domain.enum (ApifyRunStatus, Role, StatusQA)

--- src/main/java/org/example/scrapetok/repository/QuestionAndAnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.scrapetok.repository;
-
-import com.example.scrapetok.domain.QuestAndAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionAndAnswerRepository extends JpaRepository<QuestAndAnswer,Long> {
-}
